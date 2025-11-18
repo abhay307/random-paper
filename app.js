734bd7c2753d693fa50c3aa171f938e34206f32c
Abhay Bhardwaj <abhaysippy307@gmail.com>
2025-11-18 16:02:17 +0530

    motivation quotes

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 let btn = document.querySelector("button");
 let main = document.querySelector("main");
+const quotes = [
+  "Keep moving forward.",
+  "Progress starts with action.",
+  "Trust your own pace.",
+  "Discipline creates true freedom.",
+  "Dream big, start small.",
+  "Consistency builds real success.",
+  "Focus on the goal.",
+  "Small steps change everything.",
+  "Believe, act, achieve.",
+  "Create your own path.",
+];
 
-btn.addEventListener("mousemove", function () {
-  let x = Math.random() * 100;
-  let y = Math.random() * 100;
+btn.addEventListener("click", function () {
+  let x = Math.random() * 80;
+  let y = Math.random() * 80;
+  let r = Math.random() * 20;
+  let s = Math.random() * 2;
   let c1 = Math.floor(Math.random() * 256);
   let c2 = Math.floor(Math.random() * 256);
   let c3 = Math.floor(Math.random() * 256);
-  let r1 = Math.random() * 360;
-  let div = document.createElement("div");
-  div.style.width = "100px";
-  div.style.height = "100px";
-  div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
-  div.style.position = "absolute";
-  div.style.top = y + "%";
-  div.style.left = x + "%";
-  div.style.rotate = r1 + "deg";
-  main.appendChild(div);
+  let randomQuote = Math.floor(Math.random() * quotes.length);
+  let h1 = document.createElement("h1");
+  h1.innerHTML = quotes[randomQuote];
+  h1.style.position = "absolute";
+  h1.style.top = y + "%";
+  h1.style.left = x + "%";
+  h1.style.rotate = r + "deg";
+  h1.style.scale = s;
+  h1.style.color = `rgb(${c1},${c2},${c3})`;
+  main.appendChild(h1);
 });
