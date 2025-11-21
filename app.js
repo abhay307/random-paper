fa2c36fdf74eb512f343e8f6c2ace72b0a7fdd14
Abhay Bhardwaj <abhaysippy307@gmail.com>
2025-11-21 22:32:15 +0530

    card made

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const img = document.querySelector('.card img')
-const love = document.querySelector('#love')
+const users = [
+  {
+    image: "https://m.media-amazon.com/images/M/MV5BNzA2OTg5MDE2Ml5BMl5BanBnXkFtZTgwMzI0MTAwNzE@._V1_.jpg",
+    name: "John Carter",
+    position: "Senior Frontend Developer",
+    description: "Expert in React, UI architecture, and performance optimization."
+  },
+  {
+    image: "https://swimswam.com/wp-content/uploads/2023/08/366978470_969062851019460_3317365628067497678_n.jpg",
+    name: "Emily Thompson",
+    position: "UI/UX Designer",
+    description: "Creates intuitive and visually stunning user experiences."
+  },
+  {
+    image: "https://staticc.sportskeeda.com/editor/2023/05/2991b-16839198426555-1920.jpg",
+    name: "Michael Harris",
+    position: "Full Stack Developer",
+    description: "Builds scalable backend systems and seamless frontend flows."
+  },
+  {
+    image: "https://framerusercontent.com/images/ILiJVdbpGwcv26LHWlZBFATw0g.jpg",
+    name: "Sophia Martinez",
+    position: "Product Manager",
+    description: "Ensures product direction aligns with user needs and business goals."
+  },
+];
 
-img.addEventListener('dblclick', function(){
-  love.style.transform = 'translate(-50%, -50%) scale(1)'
-  love.style.opacity = 1
-  setTimeout(() => {
-      love.style.transform = 'translate(-50%, -50%) scale(0)'
-  love.style.opacity = 0  
-  },1000 );
-})
+
+let main = document.querySelector('main')
+let sum = ''
+users.forEach(element => {
+  sum = sum + `<div class="card">
+				<img src= ${element.image} alt="">
+				<h1>${element.name}</h1>
+				<h2>${element.position}</h2>
+				<p>${element.description}</p>
+				<button>Follow</button>
+			</div>`
+});
+main.innerHTML = sum
